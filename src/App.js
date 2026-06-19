068b61bf311fc2cbf61166a69488a37f7bafc454
Flavio Coulleri <104596145+flaviocoulleri@users.noreply.github.com>
2026-06-19 04:21:15 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AppLogo from './AppLogo.png'
 import './App.css';
 import BootstrapMessaging from './bootstrapMessaging';
 
-const APP_VERSION = "1.1.0";
+const APP_VERSION = "1.1.1";
 
 function App() {
   return (
