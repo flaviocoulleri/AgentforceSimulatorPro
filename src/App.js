44b711c236279bdd8e3cdb571f292bedc4d05249
Flavio Coulleri <104596145+flaviocoulleri@users.noreply.github.com>
2026-06-19 04:43:02 -0300

    Bump app version to 1.1.6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AppLogo from './AppLogo.png'
 import './App.css';
 import BootstrapMessaging from './bootstrapMessaging';
 
-const APP_VERSION = "1.1.5";
+const APP_VERSION = "1.1.6";
 
 function App() {
   return (
